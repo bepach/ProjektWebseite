ee4f862fad8b86f1fc7f12f261d3f71161d00320
bepach <162813305+bepach@users.noreply.github.com>
2024-11-12 20:53:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*function convertJSONTOTable(jsonData){
+function convertJSONTOTable(jsonData){
     let headers= Object.keys(jsonData[0]);
     let table ='<table><thead><tr>';
     headers.forEach(header => table += '<th>${header}</th>');
@@ -13,13 +13,4 @@
     document.getElementById('table-container').innerHTML = table;
 }
 
-fetch("").then(response => response.json()).then(jsonData => convertJSONTOTable(jsonData));
-*/
-
-fetch("test.json")
-.then(response => response.json())
-.then(data => showInfo(data));
-
-function showInfo(data) {
-console.table(data.countries);
-}
+fetch("company.json").then(response => response.json()).then(jsonData => convertJSONTOTable(jsonData));
